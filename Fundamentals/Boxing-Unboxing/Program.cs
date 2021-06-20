e7fb3a1776ae761d4b22a7ed08665da0179425cf
Waleed Almotairi <AlmotairiWal@gmail.com>
2021-06-20 19:12:41 +0300

    Boxing/Unboxing

---
+++ Fundamentals/Boxing-Unboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> lis = new List<object>();
+
+            lis.Add(7);
+            lis.Add(28);
+            lis.Add(-1);
+            lis.Add("chair");
+
+            int intVals = 0;
+            foreach( var item in lis)
+            {
+                Console.WriteLine(item);
+                if(item is int)
+                {
+                    intVals += (int)item;
+                }
+            }
+            Console.WriteLine(intVals);
+        }
+    }
+}
